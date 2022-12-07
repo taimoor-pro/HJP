c4242ecf343f102645340cb22bea0c34a77dc876
taimoor-pro <taimoorrnawaz@gmail.com>
2022-12-07 12:38:24 +0500

    HJP For univercity simple webbase project

---
+++ js/navbarjs.js
@@ -0,0 +1,12 @@
+const navBar = document.querySelector('.nav--page-header');
+const navBtn = document.querySelector('.nav-btn');
+navBtn.addEventListener('click', function () {
+    this.classList.toggle('open');
+    navBar.classList.toggle('nav--open');
+});
+const {
+    nav_bar = hello {
+        justify content = space if {
+            around }
+    }
+}
